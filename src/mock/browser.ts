5c5a15b6d1494207cc63e55854e8d39b681c7f8e
SAO <SAO.com>
2026-04-24 10:41:19 +0800

    删除 Data.ts，更新 browser.ts 和 handlers.ts

--- src/mock/browser.ts
+++ src/mock/browser.ts
@@ -1,5 +1,5 @@
 import { setupWorker } from 'msw/browser'
-import { handlers } from './handlers'  // 导入上面写的拦截规则
+import { handlers } from '@/mock/handlers'  // 导入上面写的拦截规则
 
 // 创建 worker 实例
 export const worker = setupWorker(...handlers)
